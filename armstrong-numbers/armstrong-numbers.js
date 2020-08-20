b2946995eff8672f7fc3a2e00423fffa0750abf1
Symeon Quimby <kershoc@gmail.com>
2020-08-19 23:34:17 -0400

    armstrong exercise

---
+++ armstrong-numbers/armstrong-numbers.js
@@ -0,0 +1,10 @@
+export const isArmstrongNumber = (subject) => {
+  let power = Math.ceil(Math.log10(subject));
+  let clone = subject;
+  let res = 0;
+  while(clone > 0) {
+      res += (clone%10)**power;
+      clone = Math.floor(clone/10);
+  }
+  return res === subject;
+};
