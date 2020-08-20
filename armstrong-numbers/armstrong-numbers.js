b4e1e377d7dd5a37d567e5c240f2a82a330d6791
Symeon Quimby <kershoc@gmail.com>
2020-08-19 23:37:53 -0400

    clean up formatting

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -2,9 +2,9 @@ export const isArmstrongNumber = (subject) => {
   let power = Math.ceil(Math.log10(subject));
   let clone = subject;
   let res = 0;
-  while(clone > 0) {
-      res += (clone%10)**power;
-      clone = Math.floor(clone/10);
+  while (clone > 0) {
+    res += (clone % 10) ** power;
+    clone = Math.floor(clone / 10);
   }
   return res === subject;
 };
