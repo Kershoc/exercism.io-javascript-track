442f0f468e2269236de5ff82e9751af4fca26d82
Symeon Quimby <kershoc@gmail.com>
2020-08-31 18:37:00 -0400

    hamming exercise

---
+++ hamming/hamming.js
@@ -0,0 +1,14 @@
+export const compute = (lstrand, rstrand) => {
+  if (lstrand.length === 0 && rstrand.length !== 0) {
+    throw new Error("left strand must not be empty");
+  }
+  if (rstrand.length === 0 && lstrand.length !== 0) {
+    throw new Error("right strand must not be empty");
+  }
+  if (lstrand.length !== rstrand.length) {
+    throw new Error("left and right strands must be of equal length");
+  }
+  let count = 0;
+  [...lstrand].map((c, i) => { if (c !== rstrand[i]) count++ });
+  return count;
+};
