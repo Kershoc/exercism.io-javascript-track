b9a607a8f5fcc8f1f6f8b15a8d491981f721936e
Symeon Quimby <kershoc@gmail.com>
2021-05-14 22:11:12 -0400

    shortened a bunch of the arrow functions

--- yacht/yacht.js
+++ yacht/yacht.js
@@ -4,75 +4,28 @@ export const score = (dice, category) => {
 };
 
 const categoryFn = {
-  'yacht': (dice) => {
-    const set = new Set(dice);
-    return (set.size === 1) ? 50 : 0
-  },
-
-  'ones': (dice) => {
-    return categoryFn.singles(dice, 1)
-  },
-
-  'twos': (dice) => {
-    return categoryFn.singles(dice, 2)
-  },
-
-  'threes': (dice) => {
-    return categoryFn.singles(dice, 3)
-  },
-
-  'fours': (dice) => {
-    return categoryFn.singles(dice, 4)
-  },
-
-  'fives': (dice) => {
-    return categoryFn.singles(dice, 5)
-  },
-
-  'sixes': (dice) => {
-    return categoryFn.singles(dice, 6)
-  },
-
-  'singles': (dice, num) => {
-    return dice.filter(e => e === num).length * num
-  },
+  'yacht': (dice) => (new Set(dice).size === 1) ? 50 : 0,
+  'ones': (dice) => categoryFn.singles(dice, 1),
+  'twos': (dice) => categoryFn.singles(dice, 2),
+  'threes': (dice) => categoryFn.singles(dice, 3),
+  'fours': (dice) => categoryFn.singles(dice, 4),
+  'fives': (dice) => categoryFn.singles(dice, 5),
+  'sixes': (dice) => categoryFn.singles(dice, 6),
+  'little straight': (dice) => categoryFn.straight(dice, 1),
+  'big straight': (dice) => categoryFn.straight(dice, 2),
+  'straight': (dice, start) => (dice.sort().every(e => e == start++)) ? 30 : 0,
+  'choice': (dice) => categoryFn.sum(dice),
+  'sum': (dice) => dice.reduce((out, elm) => out + elm, 0),
+  'singles': (dice, num) => dice.filter(e => e === num).length * num,
 
   'full house': (dice) => {
     const counts = categoryFn.getCounts(dice);
-    if ( counts.size === 2 && [...counts.values()].includes(3)) {
-      return categoryFn.sum(dice);
-    }
-    return 0;
+    return ( counts.size === 2 && [...counts.values()].includes(3)) ? categoryFn.sum(dice) : 0;
   },
 
   'four of a kind': (dice) => {
-    const counts = categoryFn.getCounts(dice);
-    const reversedCounts = new Map(Array.from(counts, a => a.reverse()));
-    if ( reversedCounts.has(4) || reversedCounts.has(5) ) {
-      return (reversedCounts.has(4) ? reversedCounts.get(4) : reversedCounts.get(5)) * 4;
-    }
-    return 0;
-  },
-
-  'little straight': (dice) => {
-    return categoryFn.straight(dice, 1);
-  },
-
-  'big straight': (dice) => {
-    return categoryFn.straight(dice, 2);
-  },
-
-  'straight': (dice, start) => {
-    if (dice.sort().every(e => e == start++)) return 30;
-    return 0;
-  },
-
-  'choice': (dice) => {
-    return categoryFn.sum(dice);
-  },
-
-  'sum': (dice) => {
-    return dice.reduce((out, elm) => out + elm, 0);
+    const rCounts = new Map(Array.from(categoryFn.getCounts(dice), a => a.reverse()));
+    return (rCounts.has(4) || rCounts.has(5)) ? (rCounts.get(4) ?? rCounts.get(5)) * 4 : 0;
   },
 
   'getCounts': (dice) => {
